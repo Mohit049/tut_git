012f9e5c8857251161675f668dd163d65b5bf532
Mohit Jajware <60690695+Mohit049@users.noreply.github.com>
2024-01-31 11:53:22 +0530

    Maintained the repo and added several new files

---
+++ Types_con.java
@@ -0,0 +1,31 @@
+public class Types_con {
+
+    public static void main(String[] args) {
+        // int binar=0b1111;
+        // System.out.println(binar);
+
+        // int hex=0x7E;
+        // System.out.println(hex);
+
+        // int num=10_000_00_000;
+        // System.out.println(num);
+
+    
+    //  int a=(int)67.5f ;
+
+    //  c=(char)a;
+    byte x=122;
+    int a=12;
+ 
+   int r=x*a;
+    //x=(byte)r;
+   
+
+    float fk=6.5f;
+
+   double dk=fk;
+     System.out.println(dk);
+
+    }
+    
+}
---
+++ first.java
@@ -0,0 +1,11 @@
+class first{
+
+
+    public static void main(String  a[]){
+
+        int n1=3;
+        int n2=7;
+        System.out.println(n1+n2);
+    } 
+
+}
