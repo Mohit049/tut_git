e952e0d4d93299ec16973eb0bebc9298d6d80b68
Mohit <mohitjajware2000@gmail.com>
2024-02-02 22:54:38 +0530

    deleted multip.java file

---
+++ datatype.java
@@ -0,0 +1,29 @@
+package tut_git;
+//int -- byte,short,int ,long
+class datatype{
+
+public static void main(String a[]){
+
+
+            byte x=18;
+            short b=200;
+            int g=1000;
+            long h=250000;
+            char c='k';
+            float k=12.67000f;
+            double y  =13.7;
+            System.out.println(x);
+        System.out.println(c);
+            System.out.println(b);
+            System.out.println(g);
+            System.out.println(h);
+        System.out.println(y);
+            System.out.println(k);
+
+           // boolean bxx=true;
+            System.out.println();
+}
+
+
+
+}
---
+++ first.java
@@ -0,0 +1,12 @@
+package tut_git;
+class first{
+
+
+    public static void main(String  a[]){
+
+        int n1=3;
+        int n2=7;
+        System.out.println(n1+n2);
+    } 
+
+}
